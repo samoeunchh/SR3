be5303f218c1c837451ce1900f550d8907bfb824
samoeun chhay <samoeunchh@gmail.com>
2021-09-25 13:40:46 +0700

    Add project files.

---
+++ SR3/Controllers/BrandsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SR3.Data;
+using SR3.Models;
+
+namespace SR3.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BrandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Brands
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Brand.ToListAsync());
+        }
+
+        // GET: Brands/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brand
+                .FirstOrDefaultAsync(m => m.BrandId == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // GET: Brands/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brands/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BrandId,BrandName,Description")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.BrandId = Guid.NewGuid();
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brand.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("BrandId,BrandName,Description")] Brand brand)
+        {
+            if (id != brand.BrandId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(brand);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandExists(brand.BrandId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brand
+                .FirstOrDefaultAsync(m => m.BrandId == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // POST: Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var brand = await _context.Brand.FindAsync(id);
+            _context.Brand.Remove(brand);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandExists(Guid id)
+        {
+            return _context.Brand.Any(e => e.BrandId == id);
+        }
+    }
+}
---
+++ SR3/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR3.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid BrandId { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage ="Brand Name field is required")]
+        [Display(Name ="Brand Name")]
+        public string BrandName { get; set; }
+        public string Description { get; set; }
+    }
+}
